beed3076363f4cbe9956d5375ac97e8b6d7563c9
pertinent23 <10franckpertinent@gmail.com>
2022-06-04 20:17:33 +0100

    after started with themes

--- modules/AppFileManager.php
+++ modules/AppFileManager.php
@@ -86,6 +86,15 @@
                 * return true if the sql
                 * global key exist 
             */
+            public static function verifyThemeExist() : bool {
+                return defined( 'themes' );
+            }
+
+            /** 
+                *
+                * return true if the sql
+                * global key exist 
+            */
             public static function verifyConfigsExist() : bool {
                 return defined( 'configs' );
             }
@@ -107,6 +116,18 @@
             public static function getUploadPath( string $name = '' ) : string {
                 if ( AppFileManager::verifyUploadsExist() ) {
                     return constant( 'uploads' ) . "$name";
+                }
+                return '';
+            }
+
+            /** 
+                *
+                * return the path to a
+                * them file path 
+            */
+            public static function getThemePath( string $name = '' ) : string {
+                if ( AppFileManager::verifyThemeExist() ) {
+                    return constant( 'themes' ) . "$name";
                 }
                 return '';
             }
--- public/utils/Tools.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    namespace App\public\utils\Tools;
-        class Tools{
-            public function isEmail( string $text ) : bool{
-                return false;
-            }
-        }
-?>
